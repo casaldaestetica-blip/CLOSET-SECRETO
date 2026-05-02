76e4a5c8a062c3da50df5928c63cae63181302b7
casaldaestetica-blip <casaldaestetica@gmail.com>
2026-05-01 22:54:25 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div style={{
+      background: "#000",
+      color: "#fff",
+      minHeight: "100vh",
+      display: "grid",
+      placeItems: "center",
+      fontFamily: "Arial"
+    }}>
+      <div style={{ textAlign: "center" }}>
+        <h1>Closet Secreto</h1>
+        <p>Black Luxury</p>
+        <p>Senha: closet1504</p>
+        <p>WhatsApp: (41) 98714-9001</p>
+      </div>
+    </div>
+  );
+}
